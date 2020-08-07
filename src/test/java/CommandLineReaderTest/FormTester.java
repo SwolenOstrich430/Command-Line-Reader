eb3f2dd30309161a678f16149efb7a76a94addab
Peter Connelly <peter@Peters-MacBook-Pro.local>
2020-08-06 21:28:16 -0400

    creating form test class

---
+++ src/test/java/CommandLineReaderTest/FormTester.java
@@ -0,0 +1,15 @@
+package CommandLineReaderTest;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+
+public class FormTester {
+
+    @Test
+    public void CreateFormFromValidPrompts() {
+
+    }
+}
